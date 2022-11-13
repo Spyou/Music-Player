8dd3a4f4a8a5e211c180529ee583a03276332d6f
Krishna Vishwakarma <krishnavishwakarma2525@gmail.com>
2022-11-13 12:47:34 +0530

    Music-player

---
+++ js/music-list.js
@@ -0,0 +1,56 @@
+let allMusic = [
+  {
+    name: "Lay On Your Love On Me",
+    artist: "ABBA",
+    img: "music-1",
+    src: "music-1"
+  },
+  {
+    name: "Middle Of The Night",
+    artist: "Elley Duhé",
+    img: "music-2",
+    src: "music-2"
+  },
+  {
+    name: "Unravel",
+    artist: "Per Fredrik Åsly",
+    img: "music-3",
+    src: "music-3"
+  },
+  {
+    name: "Kings & Queens",
+    artist: "Ava Max",
+    img: "music-4",
+    src: "music-4"
+  },
+  {
+    name: "New Genisis",
+    artist: "Singer Ado",
+    img: "music-5",
+    src: "music-5"
+  },
+  {
+    name: "Suzume",
+    artist: "Keiko Masuda",
+    img: "music-6",
+    src: "music-6"
+  },
+  {
+    name: "Blue Bird",
+    artist: "Ikimonogakari",
+    img: "music-7",
+    src: "music-7"
+  },
+  {
+    name: "House Of Memory",
+    artist: "Panic! At The Disco",
+    img: "music-8",
+    src: "music-8"
+  },
+  {
+    name: "Hikaru Nara",
+    artist: "Goose house",
+    img: "music-9",
+    src: "music-9"
+  },
+];
